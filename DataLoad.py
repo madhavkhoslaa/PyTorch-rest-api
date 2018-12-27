84e79edc70ee1a6f1f9372131fe607422da2f2a7
Madhav Khosla <madhav.khoslaa@gmail.com>
2018-12-27 14:30:49 +0530

    Create DataLoad.py

---
+++ DataLoad.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+import os
+
+class(Dataset):
+    """The DataSet is assumed to be in a hwew form.
+    DataSet Folder -> (Test and Train)
+    Test -> Different Label Folder -> Training images
+    Train -> Different Label Folders -> Training images
+    For a text data give the .csv location"""
+    def __init__(self, DFolder, transforms = None, istext = False):
+        self 
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        pass
+
+    def walk(self, DFolder, labels = False, features = False):
+        """This function returns a list of labels or features in a 
+        Dataset"""
+        Data_Dict = Dict()
+        walked = list(os.walk(DFolder))
+        for file_ in walked:
+            if os.path.isdir(file_[0]):
+                Data_Dict.update({})
+            else:
+                pass
+        if labels = True:
+            return Data_Dict.keys()
+        if features = True:
+            f = []
+            for keys, values in Data_Dict.items():
+                f.append([values[0], values[1]])
+            return f
+        else:
+            return
